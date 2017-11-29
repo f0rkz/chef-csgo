a2e6d2a4c978cdb66707ce8954f8b8182503e807
Nick Gray <f0rkz@f0rkznet.net>
2017-11-29 00:06:56 -0500

    Modify metadata and trigger a build

--- metadata.rb
+++ metadata.rb
@@ -2,22 +2,12 @@ name 'chef-csgo'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures chef-csgo'
-long_description 'Installs/Configures chef-csgo'
+description 'Installs and Configures a CSGO Dedicated Server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/chef-csgo/issues'
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/chef-csgo'
+issues_url 'https://github.com/f0rkz/chef-csgo/issues'
+source_url 'https://github.com/f0rkz/chef-csgo'
 depends 'chef-steamcmd'
 
 %w(ubuntu debian).each do |os|
