03024573c4e68f4317a65a279b0ec9fd255276d8
Nick Gray <f0rkz@f0rkznet.net>
2017-12-02 22:40:22 -0500

    Get ready for early release to chef

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'chef-csgo'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs and Configures a CSGO Dedicated Server'
+maintainer 'Nick Gray'
+maintainer_email 'f0rkz@f0rkznet.net'
+license 'Apache-2.0'
+description 'Installs/configures CSGO dedicated server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/f0rkz/chef-csgo/issues'
 source_url 'https://github.com/f0rkz/chef-csgo'
